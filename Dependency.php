23e7c0d73eed981d5b786d8691cd871d4544d9e1
Rafael <aciolyr@gmail.com>
2016-12-13 14:11:16 -0200

    Dependency class implemented

--- Dependency.php
+++ Dependency.php
@@ -2,22 +2,41 @@
 /**
  * Verify is plugin Extra Checkout Fields for Brazil are installed
  */
-function check_extra_fields_plugin_install()
-{
-   if ( ! class_exists('Extra_Checkout_Fields_For_Brazil')) {
-       add_action('admin_notices', 'birthday_dp_err');
-       return;
-   }
-}
-add_action('plugins_loaded', 'check_extra_fields_plugin_install');
-
 /**
- * Show error message asking to install plugin needed
- */
-function birthday_dp_err()
-{?>
-    <div class="error"><p>
-        <?php _e('BirthDay Celebrate requires Extra Checkout Fields for Brazil. Please install it.', 'wpse'); ?>
-    </p></div>
-<?php
+* 
+*/
+class Dependency
+{
+	public $dependencies_missing = [];
+	private $dependecies = [
+		'Extra_Checkout_Fields_For_Brazil',
+	];
+
+	public function show_missing_dependencies()
+	{
+		echo "<div class=\"error\"><ul>";
+		foreach ($this->dependencies_missing as $dependency => $value) {
+				$plugin = preg_replace('/[^a-zA-Z]/', ' ', $dependency);
+				echo "<li>";
+					_e("BirthDay Celebrate requires $plugin. Please install it.", 'wpse');
+				echo "</li>";
+		}
+		echo "</ul></div>";
+	}
+
+	public function check_dependencies()
+	{
+		foreach ($this->dependecies as $dependency) {
+			if ( ! class_exists( $dependency ) ) {
+				$this->dependencies_missing[$dependency] = true;
+			}
+		}
+
+		if (!empty($this->dependencies_missing)) {
+			add_action('admin_notices', array($this, 'show_missing_dependencies'));
+		}
+
+	}
 }
+$dependency = new Dependency();
+add_action('plugins_loaded', array($dependency, 'check_dependencies'));
