0295f5fdbc7322fc3312e027e954c3502bdc5a1a
Rafeal <aciolyr@gmail.com>
2016-12-15 21:17:58 -0200

    filter form initialized

--- includes/views/content.php
+++ includes/views/content.php
@@ -1,5 +1,18 @@
 <div class="wrap">
-    <?= __("<h3>Aniversariantes do mês</h3>"); ?>
+    <?= __("<h1>Aniversariantes do mês</h1>"); ?>
+    <form action="#">
+        <div class="tablenav top">
+            <div class="alignright actions">
+                <select name="gender" class="dropdown_product_cat">
+                    <option value="" selected="selected" disabled>Selecione o gênero</option>
+                    <option value="all">Todos</option>
+                    <option value="male">Homen</option>
+                    <option value="female">Mulher</option>
+                </select>
+                <input type="submit" name="filter_action" class="button" value="Filtrar">
+            </div>
+        </div>
+    </form>
     <table class="wp-list-table widefat fixed striped">
         <thead>
             <tr>
@@ -7,15 +20,19 @@
                 <td class="manage-column"><?= __('Usuario'); ?></td>
                 <td class="manage-column "><?= __('Data de aniversário'); ?></td>
                 <td class="manage-column column-email"><?= __('Email'); ?></td>
+                <td class="manage-column"><?= __('Detalhes'); ?></td>
             </tr>
         </thead>
         <tbody>
         <?php foreach ($customers as $customer): ?>
             <tr>
-                    <td><?= $customer->display_name ?></td>
-                    <td><?= $customer->user_nicename ?></td>
-                    <td><?= $customer->billing_birthdate->get_date()->format('d-m-Y') ?></td>
-                    <td><?= $customer->user_email ?></td>
+                <td><?= $customer->display_name ?></td>
+                <td><?= $customer->user_nicename ?></td>
+                <td><?= $customer->billing_birthdate->get_date()->format('d-m-Y') ?></td>
+                <td><?= $customer->user_email ?></td>
+                <td>
+                    <a class="button tips view" href="<?= esc_url('user-edit.php?user_id='.$customer->ID); ?>">Ver</a>
+                </td>
             </tr>
         <?php endforeach; ?>
         </tbody>
